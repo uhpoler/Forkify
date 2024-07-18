f4150f8396515809a4a4c2458962af9485eea25e
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-07-18 20:45:35 +0300

    Add update algoritm

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -13,6 +13,30 @@ export default class View {
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelector('*'));
+    const curElements = Array.from(this._parentElement.querySelector('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+
+      if (
+        newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+    });
+  }
+
   _clear() {
     this._parentElement.innerHTML = '';
   }
