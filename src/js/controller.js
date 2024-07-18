d1b24b388e83db023733bc3258e320e30e131329
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-07-18 12:40:49 +0300

    Fixed implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,17 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controllRecipes = async function () {
   try {
@@ -28,6 +32,8 @@ const controllRecipes = async function () {
 
 const controllSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -36,7 +42,8 @@ const controllSearchResults = async function () {
     await model.loadSearchResult(query);
 
     // 3) Render results
-    console.log(model.state.search.results);
+    // console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
