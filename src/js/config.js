1deb84f561015d756afc9b938df6a906c6c79fe1
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-07-19 23:12:07 +0300

    Display new user recipe

--- src/js/config.js
+++ src/js/config.js
@@ -2,3 +2,4 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 14;
 export const KEY = 'd1f52e5c-eebf-4d1b-b20d-e4d77ea62bc0';
+export const MODAL_CLOSE_SEC = 2.5;
