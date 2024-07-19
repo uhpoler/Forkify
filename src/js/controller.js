881b1e1601845930bfa0cacb61c29c95d8c79241
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-07-19 18:41:42 +0300

    Implement saving bookmarks

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,6 +3,7 @@ import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
+import bookmarksView from './views/bookmarksView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -23,6 +24,7 @@ const controllRecipes = async function () {
 
     //0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
 
     //1)Loading recipe
     await model.loadRecipe(id);
@@ -81,7 +83,7 @@ const controlAddBookmark = function () {
   recipeView.update(model.state.recipe);
 
   // 3) Render bookmarks
-  // bookmarksView.render(model.state.bookmarks);
+  bookmarksView.render(model.state.bookmarks);
 };
 
 const init = function () {
